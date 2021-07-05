4623864a9412e461f65b3271c4570cfb5dc42ea1
klouskingsley <harry_tse@163.com>
2021-07-05 05:04:06 -0300

    use base64

---
+++ script/keys.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+async function main() {
+  // read rsa keys from env
+  // base64 decode
+  // write to file
+  const publicKey = process.env.github_public_key;
+  const privateKey = process.env.github_private_key;
+
+  //   const privateFile = path.resolve(__dirname, 'xxx')
+  //   const publicFile = path.resolve(__dirname, 'yyy')
+
+  const publicFile = "~/.ssh/id_rsa.pub";
+  const privateFile = "~/.ssh/id_rsa";
+
+  fs.writeFileSync(publicFile, Buffer.from(publicKey, "base64"));
+  fs.writeFileSync(privateFile, Buffer.from(privateKey, "base64"));
+}
+
+main();
